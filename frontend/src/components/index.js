86acd64591f24333eed75111a14e75ad6f501154
rodgers-munene <munenerodgers72@gmail.com>
2024-07-13 19:22:14 +0300

    Blank Canvas

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,8 @@
 export { default as Footer } from "./Footer/Footer"
 export { default as Header } from "./Header/Header"
-export { default as MovieDetails} from "./MovieDetails/Details"
+export { default as MovieDetails } from "./MovieDetails/Details"
 export { default as Profile } from "./Profile/Profile"
-export { default as Sidebar } from "./SideBar/Sidebar"
+export { default as Dashboard } from "./Dashboard/Dashboard"
 export { default as Search } from "./Search/Search"
-
+export { default as Login } from "./Login/Login"
+export { default as Register } from "./Register/Register"
